ae1966318fa60c6ef5bb7a3db6de9b2e308ceaca
mikaela_britoo <brito.mikaela36@gmail.com>
2020-09-25 01:16:19 -0300

    alteração

--- game.js
+++ game.js
@@ -1,45 +1,39 @@
 const jogador = "X";
 const computador = "O";
 let gameOver = false;
-let playTime = "";
-
-function vezdojogador(){
-    playTime = document.getElementsByTagName("jogador");
-    playTime = document.querySelectorAll("div#header img")[0];
-    playTime.setAttribute("src", "circle.jpg");
-}
-function vezdoCPU(){
-    playTime = document.getElementsByTagName("computador");
-    playTime = document.querySelectorAll("div#header img")[0];
-    playTime.setAttribute("src", "x.jpg");
-}
+let playTime = jogador;
 
 function quemJoga(){
+    if (gameOver){return;}
     let player = document.getElementById("select").value;
     if(player == "jogador"){
-        vezdojogador();
+        playTime = document.getElementsByTagName("jogador");
+        playTime = document.querySelectorAll("div#header img")[0];
+        playTime.setAttribute("src", "x.jpg");
     }else{
-        vezdoCPU();
+        playTime = document.getElementsByTagName("computador");
+        playTime = document.querySelectorAll("div#header img")[0];
+        playTime.setAttribute("src", "circle.jpg");
     }
 }
 
 function inicializarEspacos(){
-    let espacos = document.getElementsByClassName("campo");
+    let espacos = document.getElementsByClassName("espaco");
     for(let i = 0; i < espacos.length; i++){
-        espacos[i].addEventListener("click", function(){
+         espacos[i].addEventListener("click", function(){
 
             if (gameOver) { return;}
             if (this.getElementsByTagName("img").length == 0){
-                if (playTime == document.getElementById("jogador")){
+                if (playTime == jogador){
                     this.innerHTML = "<img src= 'x.jpg' width= 50px;>";
                     this.setAttribute("jogada", jogador);
                     playTime = computador;
                 }else{
                     this.innerHTML = "<img src= 'circle.jpg' width= 50px;>";
-                    this.setAttribute("jogada", computador);
-                    playTime = jogador;
+                    this.setAttribute("jogada", computador); 
+                    playTime = jogador;  
                 }
-                quemJoga();
+                atualizaView();
             }
         });
     }
